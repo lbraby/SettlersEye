843ac4b0a57faeb8822fde497cf28bfa13fa1b41
Matthew Carbonaro <mlcarbo@nd.edu>
2023-11-26 22:06:31 -0500

    update .gitignore

--- synthesis/create_masks.py
+++ synthesis/create_masks.py
@@ -2,17 +2,15 @@
 
 import cv2
 import numpy as np
-#from skimage import measure
-import matplotlib.pyplot as plt
-import sys
+# from skimage import measure
+# import matplotlib.pyplot as plt
+# import sys
 import argparse
 import os
 import json
 
 
 def create_mask(filepath: str):
-    # Read the image into grayscale
-    print(filepath)
     sample = cv2.imread(filepath)
 
     points = read_segmentation(filepath)
@@ -22,16 +20,7 @@ def create_mask(filepath: str):
 
     cv2.drawContours(mask, [points], 0, (255, 255, 255), -1)
 
-    print(type(mask))
-    print(type(sample))
-    print(mask.shape)
-    print(sample.shape)
-    print(mask.size == sample.size)
     mask = cv2.bitwise_and(sample, sample, mask=mask)
-    print(mask.size)
-    #mask_im = cv2.resize(mask, (l, w))
-    #mask_im = cv2.resize(mask, (int(l/4), int(w/4)))
-
 
     index = len(filepath) - 1
     while filepath[index] != ".":
@@ -40,14 +29,18 @@ def create_mask(filepath: str):
     root = filepath[:index]
     ext = filepath[index:]
     path = root + "_mask" + ext
-    print(path)
 
-    print(cv2.imwrite(path, mask))
+    print(f"Successfully wrote {path}: ", cv2.imwrite(path, mask))
 
-    #cv2.imshow("mask", mask_im)
+    # Uncomment to see image
+    '''
+    mask_im = cv2.resize(mask, (l//4, w//4))
 
+    cv2.imshow("mask", mask_im)
 
-    #cv2.waitKey()
+    cv2.waitKey()
+    '''
+
 
 def read_segmentation(filepath: str) -> np.array:
     index = len(filepath) - 1
@@ -66,114 +59,13 @@ def read_segmentation(filepath: str) -> np.array:
     return points
 
 
-
-'''
-    # Convert the original image to HSV
-    # and take H channel for further calculations
-    sample_hsv = cv2.cvtColor(sample, cv2.COLOR_BGR2HSV)
-    sample_h = sample_hsv[:, :, 0]
-
-    # Uncomment this to show the H channel of the image
-    #sample_small = cv2.resize(sample_h, (640, 480))
-    #print('H channel of the image')
-    #cv2.imshow(sample_small)
-
-
-    # *** TASK *** It's a good place to apply morphological operations to the "binary_image"
-    # definition of a kernel (a.k.a. structuring element):
-    im_floodfill = binary_image.copy()
-    h, w = binary_image.shape[:2]
-    mask = np.zeros((h+2,w+2), np.uint8)
-    cv2.floodFill(im_floodfill, mask, (0, 0), 255)
-    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
-    binary_image = binary_image | im_floodfill_inv
-
-    kernel = np.ones((19, 19),np.uint8)
-    binary_image = cv2.erode(src=binary_image, kernel=kernel, iterations = 1)
-
-    # Uncomment this to show image after morphological transformation
-    #sample_small = cv2.resize(sample_res, (640, 480))
-    #print('Image after morphological transformation',sample_small)
-    #cv2.imshow(sample_small)
-
-    # Find connected pixels and compose them into objects
-    labels = measure.label(binary_image)
-
-    # Calculate features for each object; since we want to differentiate
-    # between circular and square shapes, the major and minor axes may be less helpful
-    # so think what other features could be useful; we will use also the centroid
-    # to annotate the final result
-    properties = measure.regionprops(labels)
-
-
-
-    # *** TASK *** Calculate features for each object:
-    # - some geometrical feature 1 (dimension 1)
-    # - some intensity/color-based feature 2 (dimension 2)
-    features = np.zeros((len(properties), 2))
-    for i in range(0, len(properties)):
-        features[i, 0] = properties[i].perimeter
-        min_row, min_col, max_row, max_col = properties[i].bbox
-        single_object_hue = sample_h[min_row:max_row,min_col:max_col]
-
-        # calculating the average intensity of each image patch
-        features[i, 1] = np.mean(single_object_hue)
-
-
-
-    # *** TASK *** Show our objects in the feature space
-    plt.plot(features[:, 0],features[:, 1], 'ro')
-    plt.xlabel('Feature 1: perimeter')
-    plt.ylabel('Feature 2: hue')
-    plt.show()
-
-
-
-    # *** TASK *** Choose the threshold
-    thrF1 = 250
-    thrF2 = 50
-
-
-
-    # It's time to classify, count and display the objects
-    squares = 0
-    blue_circles = 0
-    red_circles = 0
-
-    fig, ax = plt.subplots()
-    ax.imshow(cv2.cvtColor(sample, cv2.COLOR_BGR2RGB))
-
-
-
-    # *** TASK *** Code your classification here (using calculated features):
-    for i in range(0, len(properties)):
-        if (features[i, 0] > thrF1 and features[i, 1] <= thrF2):
-        squares = squares + 1
-        ax.plot(np.round(properties[i].centroid[1]), np.round(properties[i].centroid[0]), '.g', markersize=15)
-
-        if (features[i, 0] <= thrF1 and features[i, 1] > thrF2):
-        blue_circles = blue_circles + 1
-        ax.plot(np.round(properties[i].centroid[1]), np.round(properties[i].centroid[0]), '.b', markersize=15)
-
-        if (features[i, 0] <= thrF1 and features[i, 1] <= thrF2):
-        red_circles = red_circles + 1
-        ax.plot(np.round(properties[i].centroid[1]), np.round(properties[i].centroid[0]), '.r', markersize=15)
-    plt.show()
-
-
-
-    # That's all! Let's display the result:
-    # print("I found %d squares, %d blue donuts, and %d red donuts." % (squares, blue_circles, red_circles))
-'''
-
 def build_parser():
     parser = argparse.ArgumentParser(description="Python utility to get catan piece object masks.")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--filename", help="Image to create mask of")
-    group.add_argument("--dir", metavar="DIRECTORY", help="directory to make mask of all images in", required=False)
+    group.add_argument("--filename", '--file', '-f', help="Image to create mask of")
+    group.add_argument("--dir", '--directory', '-d', metavar="DIRECTORY", help="directory to make mask of all images in", required=False)
 
     return parser
-
 
 
 def main():
