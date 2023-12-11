971d74e5223249cd3daa89398238ab41ba12aa5d
Matthew Carbonaro <mcarbona@nd.edu>
2023-12-11 16:34:50 -0500

    Make tabulation function

--- YOLO/model.py
+++ YOLO/model.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 from ultralytics import YOLO
+from ultralytics.engine import results
 import argparse
 from supervision import Detections, BoxAnnotator, ColorPalette
 import cv2
+from collections import defaultdict
+from typing import Dict
 
 def build_parser():
     parser = argparse.ArgumentParser(description="Python wrapper to run YOLOv8 model on images")
@@ -14,10 +17,44 @@ def build_parser():
 
     return parser
 
+def check_piece_bounds(center: list, bboxes: list):
+    for bbox in bboxes:
+        if in_bounds(center, bbox):
+            return False
+
+    return True
+
+def in_bounds(center: list, bbox: list):
+    if center[0] >= bbox[0] and center[1] >= bbox[1] and center[0] >= bbox[2] and center[1] >= bbox[3]:
+        return True
+
+    return False
+
+def tabulate_pieces(m_result: results):
+    translation = m_result.names
+    box_results = m_result.boxes
+    bboxes = []
+    print(box_results)
+
+    scores: Dict[str, Dict[str, int]] = defaultdict(lambda: defaultdict(int))
+
+    for index, identification in enumerate(box_results.cls.numpy().astype(int)):
+        color, piece = translation[identification].split("-")
+
+        if in_bounds(box_results.xywh[index][:2], box_results.xyxy[index]):
+            continue
+        else:
+            bboxes.append(box_results.xyxy[index])
+
+        scores[color][piece] += 1
+
+    return scores
+
+
+
 def main():
     parser = build_parser()
     args = parser.parse_args()
-    print(args)
     model = YOLO(args.model)
     box_annotator = BoxAnnotator(color=ColorPalette.default(), thickness=4, text_thickness=4, text_scale=2)
     results = model.predict(args.image, imgsz=args.image_size, conf=args.confidence)[0]
@@ -27,9 +64,9 @@ def main():
                             )
 
     frame = box_annotator.annotate(scene=results.orig_img, detections=detections)
-    print(results)
-    print(results.boxes)
     frame = cv2.resize(frame, (640, 640))
+    scores = tabulate_pieces(results)
+    print(scores)
     cv2.imshow("Annotated Board", frame)
     cv2.waitKey(0)
 
